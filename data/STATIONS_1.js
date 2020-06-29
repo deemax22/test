3635869d92e4ba0ce42d95990a2eb3f6916375af
deemax22 <65885524+deemax22@users.noreply.github.com>
2020-06-29 22:06:54 +0800

    Add files via upload

---
+++ data/STATIONS_1.js
@@ -0,0 +1,8 @@
+var json_STATIONS_1 = {
+"type": "FeatureCollection",
+"name": "STATIONS_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Layer": "ALINGMENT", "PaperSpace": null, "SubClasses": "AcDbEntity:AcDbPolyline", "Linetype": null, "EntityHand": "11", "Text": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 120.960478347041231, 14.455647598741459 ], [ 120.960571056735674, 14.455650360305567 ], [ 120.960663766432518, 14.45565312183318 ], [ 120.960697772882739, 14.45456901845524 ], [ 120.960605063634816, 14.4545662569419 ], [ 120.960512354389309, 14.454563495392069 ], [ 120.960478347041231, 14.455647598741459 ] ] ] ] } }
+]
+}
